29379d08a760094d7d7827fbcf77751fedd459c6
kostino <triaridis@ece.auth.gr>
2021-03-04 18:56:27 +0200

    Add basic cmd line options

--- calculator.py
+++ calculator.py
@@ -1,12 +1,15 @@
 import pandas as pd
-import sys
+import argparse
 from xpoints import xpoints
 from prepare_games import prepare_games
 
 def main():
-    if len(sys.argv) != 2:
-        print('Incorrect usage!. Use as:\n python3 calculator.py <results_file.csv>')
-    print(sys.argv[1])
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_data",help="CSV file containing input data",type=ascii)
+    parser.add_argument("-i","--img",help="Generate image for xpoints table",action="store_true")
+    parser.add_argument("-j","--json",help="Save xpoints data as JSON",action="store_true")
+    parser.add_argument("-c","--csv",help="Save xpoints data as CSV",action="store_true")
+    args = parser.parse_args()
     results = pd.read_csv(sys.argv[1])
     results = prepare_games(results)
     xpoints_dict = xpoints(results)
