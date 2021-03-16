07fc3cd35e55a282335c263e05bdb692c28db283
kostino <triaridis@ece.auth.gr>
2021-03-16 17:42:32 +0200

    Fix bug

--- prepare_games.py
+++ prepare_games.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def prepare_games(matchlist):
-    matchlist = matchlist[['Home','xGHome','Score','xGAway','Away']]
+    matchlist = matchlist[['Home','xGHome','xGAway','Away']]
     matchlist=matchlist[matchlist['xGHome'].notna()]
     return matchlist
 
